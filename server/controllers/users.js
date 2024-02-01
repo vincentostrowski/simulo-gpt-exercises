c6296134f7a65a7577c194eeb5048e01e4419755
vincentostrovski <92182422+vincentostrovski@users.noreply.github.com>
2024-02-01 15:19:26 -0500

    Break createUserInFirebase and createUserInMongo functions from createUser

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,46 +1,51 @@
 const User = require("../models/user");
 const admin = require("../utils/firebaseAdmin");
 
+const createUserInFirebase = async ({ email, password, username }) => {
+  return await admin.auth().createUser({
+    email,
+    password,
+    displayName: username,
+  });
+};
+
+const createUserInMongo = async ({ username, email, fireBaseId }) => {
+  const user = new User({
+    username,
+    email,
+    fireBaseId,
+  });
+  return await user.save();
+};
+
 const createUser = async (request, response) => {
   const { username, email, password } = request.body;
-
-  //creating both user in Mongo & FireBase
   try {
-    //Firebase user creation
-    const userRecord = await admin.auth().createUser({
+    const userSaved = await createUserInFirebase({ email, password, username });
+    const userRecord = await createUserInMongo({
       email,
-      password,
-      displayName: username,
+      fireBaseId: userSaved.uid,
+      username,
     });
-
-    //Mongo user creation. If fails, delete Firebase user
-    try {
-      const user = new User({
-        username,
-        email,
-        fireBaseId: userRecord.uid,
-      });
-
-      const savedUser = await user.save();
-      response.status(201).json(savedUser);
-    } catch (mongoError) {
-      await admin.auth().deleteUser(userRecord.uid);
-      if (mongoError.code === 11000) {
-        return response.status(400).json({ error: "Username already in use" });
-      } else {
-        return response.status(500).json({
-          error:
-            "An error occurred while creating your account. Please try again.",
-        });
-      }
-    }
+    response.status(201).json(userRecord);
   } catch (error) {
-    if (error.code === "auth/email-already-exists") {
+    if (userSaved) {
+      await admin.auth().deleteUser(userSaved.uid);
+    }
+    if (error.code === 11000) {
+      return response.status(400).json({ error: "Username already in use" });
+    } else if (error.code === "auth/email-already-exists") {
       return response.status(400).json({ error: "Email already in use" });
     } else if (error.code === "auth/invalid-email") {
       return response.status(400).json({ error: "Invalid email" });
     } else if (error.code === "auth/weak-password") {
       return response.status(400).json({ error: "Weak password" });
+    } else {
+      console.log(error);
+      return response.status(500).json({
+        error:
+          "An error occurred while creating your account. Please try again.",
+      });
     }
   }
 };
